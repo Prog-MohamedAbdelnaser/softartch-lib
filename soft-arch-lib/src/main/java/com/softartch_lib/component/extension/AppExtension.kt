dbea1f82ff8b27ad1ece9cf331fa6986db9c2809
DESKTOP-G2SJ2ON\mohamedabdelnaser <mohamedabdelnaser782@gmail.com>
2022-03-31 13:02:08 +0200

    #resolve issues

--- soft-arch-lib/src/main/java/com/softartch_lib/component/extension/AppExtension.kt
+++ soft-arch-lib/src/main/java/com/softartch_lib/component/extension/AppExtension.kt
@@ -1,14 +1,6 @@
 package com.softartch_lib.component.extension
 
-import android.app.Activity
-import android.content.DialogInterface
-import android.content.Intent
-import android.net.ConnectivityManager
-import android.util.Log
 import android.view.View
-import androidx.fragment.app.Fragment
-import com.softartch_lib.utility.TimeConvertor
-import org.koin.android.ext.android.inject
 import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.util.*
